2f2e6ab1e04303259fa6100226a04d974f3a0200
sandstranger <sandstranger@gmail.com>
2016-12-21 21:18:47 +0400

    add media codecs from lenovo

--- device.mk
+++ device.mk
@@ -138,6 +138,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml
 
 PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
 	$(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
 	$(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 
